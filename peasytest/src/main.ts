567aafb83acefe8892499a7998e1d0e61b5ab729
Jürgen Wenzel <jurgen@jwenzel.se>
2023-03-09 00:26:39 +0100

    Fix demo

--- peasytest/src/main.ts
+++ peasytest/src/main.ts
@@ -3,18 +3,31 @@ import { UI } from "@peasy-lib/peasy-ui";
 
 type testRecordtype = Record<string, Array<object>>;
 
+// If you prefer class model:
+// class Model {
+//   public myDir: testRecordtype = {};
+//   public testkey: 'test' | 'demo' = 'demo';
+//   public get list() {
+//     return this.myDir[this.testkey];
+//   }
+// }
+// const model = new Model();
+
 const model = {
   myDir: <testRecordtype>{},
-};
+  testkey: 'demo',
+  get list() {
+    return model.myDir[model.testkey];
+  }
+
+}
 
 model.myDir.test = [];
-model.myDir.demo = [{ 0: { x: 0, y: 0, str: "Hello" } }, { 1: { x: 1, y: 2, str: "World" } }];
-
-let testkey = "test";
+model.myDir.demo = [{ id: 0, x: 0, y: 0, str: "Hello" }, { id: 1, x: 1, y: 2, str: "World" }];
 
 const template = `
-<div class="app">
-  <div class="record" \${rec<=*myDir.myRecord[${testkey}]:id}></div>
+<div class="app">\${testkey}:
+  <div class="record" \${rec <=* list:id}>\${rec.str}</div>
 </div>
 `;
 
@@ -22,8 +35,6 @@ UI.create(document.body, template, model);
 UI.initialize(100 / 6);
 
 setInterval(() => {
-  console.log(testkey);
-
-  if (testkey == "test") testkey = "demo";
-  else testkey = "test";
+  if (model.testkey == "test") model.testkey = "demo";
+  else model.testkey = "test";
 }, 2000);
