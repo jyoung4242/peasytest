71db2053b9052c3ad75a7509c3086842a67fe033
Justin Young <justin_dean_young@yahoo.com>
2023-03-08 17:53:07 -0500

    init

---
+++ peasytest/src/main.ts
@@ -0,0 +1,29 @@
+import "./style.css";
+import { UI } from "@peasy-lib/peasy-ui";
+
+type testRecordtype = Record<string, Array<object>>;
+
+const model = {
+  myDir: <testRecordtype>{},
+};
+
+model.myDir.test = [];
+model.myDir.demo = [{ 0: { x: 0, y: 0, str: "Hello" } }, { 1: { x: 1, y: 2, str: "World" } }];
+
+let testkey = "test";
+
+const template = `
+<div class="app">
+  <div class="record" \${rec<=*myDir.myRecord[${testkey}]:id}></div>
+</div>
+`;
+
+UI.create(document.body, template, model);
+UI.initialize(100 / 6);
+
+setInterval(() => {
+  console.log(testkey);
+
+  if (testkey == "test") testkey = "demo";
+  else testkey = "test";
+}, 2000);
